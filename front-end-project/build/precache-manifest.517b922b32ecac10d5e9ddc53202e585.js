1f4906d7faa1ab84c6d4fffd7055a4080814c614
nikith2694 <53906464+nikith2694@users.noreply.github.com>
2019-08-09 11:19:50 +0530

    Add files via upload

---
+++ front-end-project/build/precache-manifest.517b922b32ecac10d5e9ddc53202e585.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bb4813205eb8e292b06174e3351d513",
+    "url": "/index.html"
+  },
+  {
+    "revision": "149c3cf7045862cb2653",
+    "url": "/static/css/main.31c00375.chunk.css"
+  },
+  {
+    "revision": "ff07cfb111d1053e76d2",
+    "url": "/static/js/2.3cb00523.chunk.js"
+  },
+  {
+    "revision": "149c3cf7045862cb2653",
+    "url": "/static/js/main.14fc5c4c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
